26d06d3cd45f0f42895526d19d0677edffc71a4d
chikela <153768217+chikela@users.noreply.github.com>
2024-01-16 04:48:02 +0300

    Update config.keys.ts

--- client/src/config.keys.ts
+++ client/src/config.keys.ts
@@ -1,9 +1,9 @@
 export const CLIENT_URL =
-  import.meta.env.VITE_CLIENT_URL || 'http://localhost:3000';
+  import.meta.env.VITE_CLIENT_URL || 'https://client-sqpa.onrender.com';
 export const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
+  import.meta.env.VITE_SERVER_URL || 'https://vita-qy1k.onrender.com';
 export const SOCKET_URL =
-  import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+  import.meta.env.VITE_SOCKET_URL || 'https://vita-qy1k.onrender.com';
 export const PRODUCTION = import.meta.env.VITE_PROD === 'true';
 export const GOOGLE_ANALYTICS_KEY =
   import.meta.env.VITE_GOOGLE_ANALYTICS_KEY || 'UA-000000000-0';
