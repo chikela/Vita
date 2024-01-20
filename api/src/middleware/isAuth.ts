7b812005b5cb78106d400615840fb6444e989b5d
chikela <153768217+chikela@users.noreply.github.com>
2024-01-21 02:25:12 +0300

    Update isAuth.ts

--- api/src/middleware/isAuth.ts
+++ api/src/middleware/isAuth.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
  console.log("ISauth");
-  console.log(req.cookies.vitaa-session);
+  console.log(req.session.name);
   if (!req.user) {
     return res.status(401).json({
       error: 'You are not authenticated!',
