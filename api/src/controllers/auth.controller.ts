94674f4ec952eb3fa1d9286299fbf13ab37f6a32
chikela <153768217+chikela@users.noreply.github.com>
2024-01-25 03:20:34 +0300

    Update auth.controller.ts

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -415,7 +415,7 @@ const logout = (req: Request, res: Response) => {
 const registerUser = async (req: Request, res: Response) => {
   const data = parseFormData(req.body);
 console.log("ON REGISTER");
-  //console.log(res);
+  console.log(req.headers.cookie);
   console.log(req.user);
   if (!req.user) {
     return res.status(401).json({
