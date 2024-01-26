4762ac6b86ab229631ca65698386937d670860cb
chikela <153768217+chikela@users.noreply.github.com>
2024-01-26 04:33:43 +0300

    Update auth.controller.ts

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -183,13 +183,13 @@ const jwtLogin = async (req: Request, res: Response) => {
   }
 
   const token = user.issueToken();
-  res.setHeader('Set-Cookie', token);
+  // res.setHeader('Set-Cookie', token);
 //console.log(token);
   res.cookie('jwt', token, {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    domain: 'www.chikela.et',
-    sameSite: 'none',
+    //domain: 'www.chikela.et',
+    //sameSite: 'none',
   });
   return res.status(200).json({ isLoggedIn: true, user });
 };
