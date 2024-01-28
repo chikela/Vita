2827c1dee8ad5064da08c3b094d862c959a4f190
chikela <153768217+chikela@users.noreply.github.com>
2024-01-28 06:16:57 +0300

    Update isAuth.ts

--- api/src/middleware/isAuth.ts
+++ api/src/middleware/isAuth.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
  console.log("ISauth");
  //const token = req.headers.authorization.split(" ")[1];
-console.log(req.user);
+console.log(req.cookies.jwt);
   if (!req.user) {
     return res.status(401).json({
       error: 'You are not authenticated!',
